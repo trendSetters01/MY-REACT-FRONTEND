72cd4bdbbba56c98e4bc0d6c5808c4b6fa02c3d5
trendSetters01 <trendycrypto01@gmail.com>
2023-12-15 08:59:01 -0500

    fix: game header

--- src/components/MainGame/index.js
+++ src/components/MainGame/index.js
@@ -10,7 +10,11 @@ export default function MainGame({ accountAddress }) {
       style={{ height: "80vh" }}
       className="flex flex-col items-center justify-center text-white"
     >
-      <h1 className="text-4xl text-center font-bold text-white mb-20">Welcome To Cards Adventure!</h1>
+      {accountAddress && (
+        <h1 className="text-4xl text-center font-bold text-white mb-20">
+          Welcome To Cards Adventure!
+        </h1>
+      )}
       {!accountAddress && (
         <h1 className="animate-pulse text-white">
           Connect your wallet to participate.
