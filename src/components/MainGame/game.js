b0bedbcb885510d164ccf1eb89ef829e7a33732e
trendSetters01 <trendycrypto01@gmail.com>
2023-12-16 04:22:37 -0500

    refactor: updated info

--- src/components/MainGame/game.js
+++ src/components/MainGame/game.js
@@ -194,14 +194,18 @@ export default function GameComponent({ onDepositSuccess, accountAddress }) {
       {showConfetti && <Confetti width={window.width} height={window.height} />}
       {accountAddress && (
         <div className="w-full max-w-4xl bg-light rounded-lg p-4 text-white">
-          <h1 className="text-4xl text-center font-bold text-white mb-8 w-80">
-            Welcome To Cards RPG!
-          </h1>
-          <p className="mb-4 w-80">
-            An evolving auto play rpg. You will play against an AI. If you win,
-            you will be rewarded with 1 Reedemable NFT for future prizes when
-            the game comes out of test phase.....
-          </p>
+          {!(aiLog.length > 0) && !(userLog.length > 0) && (
+            <div>
+              <h1 className="text-4xl text-center font-bold text-white mb-8 w-80">
+                Welcome To Cards RPG!
+              </h1>
+              <p className="mb-4 w-80">
+                An evolving auto play rpg. You will play against an AI. If you
+                win, you will be rewarded with 1 Reedemable NFT, for future
+                prizes, When the game comes out of test phase.....
+              </p>
+            </div>
+          )}
         </div>
       )}
       <div>
