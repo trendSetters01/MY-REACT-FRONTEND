2bd97f7fa39097b36b889023af0699828a9b2b01
trendSetters01 <trendycrypto01@gmail.com>
2023-12-17 01:35:24 -0500

    refactor

--- src/algorand/config.js
+++ src/algorand/config.js
@@ -3,13 +3,20 @@ import algosdk from "algosdk";
 const ALGOD_API_ADDR = "https://mainnet-algorand.api.purestake.io/ps2";
 const ALGOD_INDEXER_ADDR = "https://mainnet-algorand.api.purestake.io/idx2";
 const ALGOD_PORT = "";
-const PURESTAKE_API_KEY = process.env["PURESTAKE_API_KEY"];
+const REACT_APP_PURESTAKE_API_KEY = process.env.REACT_APP_PURESTAKE_API_KEY;
 const ALGOD_API_TOKEN = {
-  "X-API-Key": PURESTAKE_API_KEY,
+  "X-API-Key": REACT_APP_PURESTAKE_API_KEY,
 };
 
-
-const algodClient = new algosdk.Algodv2(ALGOD_API_TOKEN, ALGOD_API_ADDR, ALGOD_PORT);
-const algoIndexerClient = new algosdk.Indexer(ALGOD_API_TOKEN, ALGOD_INDEXER_ADDR, ALGOD_PORT);
+const algodClient = new algosdk.Algodv2(
+  ALGOD_API_TOKEN,
+  ALGOD_API_ADDR,
+  ALGOD_PORT
+);
+const algoIndexerClient = new algosdk.Indexer(
+  ALGOD_API_TOKEN,
+  ALGOD_INDEXER_ADDR,
+  ALGOD_PORT
+);
 
 export { algodClient, algoIndexerClient };
