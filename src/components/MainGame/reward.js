fa1fbe708540976f60b64674b3f2b1f61f2d474c
trendSetters01 <trendycrypto01@gmail.com>
2023-12-19 02:00:10 -0500

    fix: reward cliam

--- src/components/MainGame/reward.js
+++ src/components/MainGame/reward.js
@@ -8,7 +8,6 @@ import { getRandomNFTAssetId } from "../../algorand/transactionHelpers/getRandom
 
 import { PeraWalletContext } from "../PeraWalletContext";
 import { autoOptOutRewardedAsset } from "../../algorand/opt-out.js";
-import { checkOptIn } from "../../algorand/transactionHelpers/checkOptInStatus.js";
 
 export default function RewardComponent({ accountAddress }) {
   const [status, setStatus] = useState("");
@@ -17,8 +16,7 @@ export default function RewardComponent({ accountAddress }) {
   const [transactionId, setTransactionId] = useState(null);
   const [showMsg, setShowMsg] = useState(false);
   const [rewardAmount, setRewardAmount] = useState(0);
-  const [showRewardButton, setshowRewardButton] = useState(false);
-  const [showOptInButton, setshowOptInButton] = useState(true);
+  const [showRewardButton, setshowRewardButton] = useState(true);
   const [nftCount, setNftCount] = useState(0);
   const [boostMultiplier, setBoostMultiplier] = useState(1);
   const BASE_PHNTM_REWARD = 0;
@@ -26,12 +24,6 @@ export default function RewardComponent({ accountAddress }) {
   const peraWallet = useContext(PeraWalletContext);
   const phantomsHoldingAddress =
     "XGJS5VTFTVB3MJDQGXH4Y4M6NYDYEK4OZFF6NIVUTIBS52OTLW2N5CYM2Y";
-
-  useEffect(() => {
-    if (showOptInButton) {
-      handleOptIn();
-    }
-  }, []);
 
   function calculateRewards(baseReward, nftCount) {
     let multiplier = 1;
@@ -46,48 +38,8 @@ export default function RewardComponent({ accountAddress }) {
     return baseReward * multiplier;
   }
 
-  async function handleOptIn() {
-    // setStatus("Opt-in Processing...");
-    try {
-      const assetID = await getRandomNFTAssetId(phantomsHoldingAddress);
-
-      // const txn = await optIn(
-      //   accountAddress, //"1276228104"
-      //   assetID
-      // );
-      // const signedTx = await peraWallet.signTransaction([txn]);
-      // const txConfirmation = await algodClient
-      //   .sendRawTransaction(signedTx)
-      //   .do();
-      // console.log("Transaction ID:", txConfirmation.txId);
-      // setStatus(
-      //   "Opt-in successful, please click the claim reward button to initiate the transfer process."
-      // );
-      setshowOptInButton(false);
-      setAssetID(assetID);
-      setshowRewardButton(false);
-      const assetId = assetID;
-      const interval = 30000; // 30 seconds
-      const duration = 5 * 60 * 1000; // 5 minutes
-
-      const optedIn = await pollForOptIn(
-        accountAddress,
-        assetId,
-        interval,
-        duration
-      );
-      if (optedIn) {
-        setshowRewardButton(true);
-      }
-    } catch (error) {
-      console.log("Couldn't sign Opt-in txn", error);
-      setStatus("Opt-in failed");
-      setAssetID(null);
-    }
-  }
-
   async function handleGameWin() {
-    // setshowRewardButton(false);
+    setshowRewardButton(false);
     setShowLoader(true);
     try {
       const nftCount = await countPhntmNfts(accountAddress);
@@ -97,29 +49,43 @@ export default function RewardComponent({ accountAddress }) {
       setNftCount(nftCount);
       setRewardAmount(rewardAmount);
 
+      const assetID = await getRandomNFTAssetId(phantomsHoldingAddress);
+      setAssetID(assetID);
+
+      const optInTxn = await optIn(
+        accountAddress, //"1276228104"
+        assetID
+      );
+
       // reward distribution logic
       // setStatus("Initiating reward distribution...");
       const txn = await send(
         phantomsHoldingAddress,
         accountAddress,
         totalPhantomTokenConversion,
-        assetID, //"1276228104", // for phntm thnx token
+        assetID,
         "Thank you for testing the game!"
       );
 
-      const signedTx = await peraWallet.signTransaction([txn]);
-      const txConfirmation = await algodClient
-        .sendRawTransaction(signedTx)
-        .do();
+      const signedTx = await peraWallet.signTransaction([optInTxn, txn]);
 
-      console.log("Transaction ID:", txConfirmation.txId);
+      for (const signedTxnGroup of signedTx) {
+        const { txId } = await algodClient
+          .sendRawTransaction(signedTxnGroup)
+          .do();
+
+        console.log(`txns signed successfully! - txID: ${txId}`);
+        setTransactionId(txId);
+      }
+
+      // console.log("Transaction ID:", txConfirmation.txId);
       setTimeout(async () => {
         // opt out of the rewarded asset if amount of that asset held is zero
         await autoOptOutRewardedAsset(assetID);
         setShowLoader(false);
-        setTransactionId(txConfirmation.txId);
+        // setTransactionId(txConfirmation.txId);
+        setStatus(`Reward distribution completed`);
       }, 10000);
-      // setStatus(`Reward distribution completed`);
 
       if (rewardAmount > 0) {
         setShowMsg(true);
@@ -138,52 +104,16 @@ export default function RewardComponent({ accountAddress }) {
     }
   }
 
-  async function pollForOptIn(address, assetId, interval, duration) {
-    const endTime = Date.now() + duration;
-
-    return new Promise((resolve, reject) => {
-      const check = async () => {
-        try {
-          if (await checkOptIn(address, assetId)) {
-            console.log("User has opted in!");
-            resolve(true);
-          } else if (Date.now() < endTime) {
-            setTimeout(check, interval);
-          }
-        } catch (error) {
-          reject(error);
-        }
-      };
-
-      check();
-    });
-  }
-
   return (
     <div className="flex flex-col items-center justify-center mb-4">
       <div class="flex flex-col items-center">
-        <div
-          style={{ paddingBottom: "3em" }}
-          // onClick={handleOptIn}
-          className="mt-4 input-md bg-gradient-to-r from-red-500 to-orange-600 hover:from-orange-600 hover:to-red-500 rounded-md text-lg"
-          disabled={!showOptInButton}
+        <button
+          onClick={handleGameWin}
+          className="mt-8 input-md bg-gradient-to-r from-purple-500 to-blue-400 hover:from-blue-400 hover:to-purple-500 rounded-md"
+          disabled={!showRewardButton}
         >
-          <p>Please Opt In to {assetID} To Receive Reward. </p>
-        </div>
-
-        <p className="mt-2">
-          Then wait for the claim reward button to show up!
-        </p>
-
-        {showRewardButton && (
-          <button
-            onClick={handleGameWin}
-            className="mt-8 input-md bg-gradient-to-r from-purple-500 to-blue-400 hover:from-blue-400 hover:to-purple-500 rounded-md"
-            disabled={!showRewardButton}
-          >
-            Claim Reward
-          </button>
-        )}
+          Claim Reward
+        </button>
         {showLoader && (
           <span className="mt-4 loading loading-spinner loading-lg text-white">
             <div class="flex justify-center">
