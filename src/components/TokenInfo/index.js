d8a911e4e36ecd79f0874e2e8ffae591ee0bec96
trendSetters01 <trendycrypto01@gmail.com>
2024-01-22 02:58:05 -0500

    refactor: style

--- src/components/TokenInfo/index.js
+++ src/components/TokenInfo/index.js
@@ -13,7 +13,28 @@ function TokenInfo({ accountAddress }) {
       {activeTab === "Token Info" && (
         <section className="flex flex-col items-center justify-center bg-gradient-to-r from-black to-gray-500 text-white shadow-lg">
           {!iframeLoading && (
-            <h2 className="text-2xl font-semibold">Token Information</h2>
+            <div
+              style={{ padding: "0.125rem", maxHeight: "24vh" }}
+              role="alert"
+              className="alert alert-info flex flex-col items-center justify-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="stroke-current shrink-0 w-6 h-6 pt-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+              <span style={{ fontSize: "small" }} className="pb-4 pl-4 pr-4">
+                Token Information
+              </span>
+            </div>
           )}
           {iframeLoading && (
             <span className="loading loading-spinner loading-lg text-white">
--- src/components/VestigeListing/index.js
+++ src/components/VestigeListing/index.js
@@ -13,11 +13,33 @@ function VestigeListing({ accountAddress }) {
       {activeTab === "Vestige Listing" && (
         <section className="flex flex-col items-center justify-center bg-gradient-to-r from-black to-gray-500 text-white shadow-lg">
           {!iframeLoading && (
-            <h1 className="animate-pulse text-red-500 p-4">
-              Vestige wallet connect is a different session than the one on this
-              website. Please connect your wallet to Vestige. Click the Connect
-              button, to interact with their swaps.
-            </h1>
+            <div
+              style={{ padding: "0.125rem", maxHeight: "24vh" }}
+              role="alert"
+              className="alert alert-warning flex flex-col items-center justify-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="stroke-current shrink-0 h-6 w-6 pt-2"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                />
+              </svg>
+              <span
+                style={{ fontSize: "small" }}
+                className="animate-pulse pb-4 pl-4 pr-4"
+              >
+                Vestige wallet connect is a different session than the one on
+                this website. Please connect your wallet to Vestige. Click the
+                Connect button, to interact with their swaps.
+              </span>
+            </div>
           )}
           {iframeLoading && (
             <span className="loading loading-spinner loading-lg text-white">
