7af6f7ef811f883c5f883aaf87a665b5f59309a9
trendSetters01 <trendycrypto01@gmail.com>
2024-05-08 00:33:52 -0400

    refactor: raffle styles

--- src/components/Raffle/index.js
+++ src/components/Raffle/index.js
@@ -16,10 +16,25 @@ const Raffle = ({ accountAddress }) => {
   const [transactionId, setTransactionId] = useState("");
   const [showLoader, setShowLoader] = useState(false);
   const [disabled, setDisabled] = useState(false);
+  const [raffleResults, setRaffleResults] = useState([]);
 
   const peraWallet = useContext(PeraWalletContext);
   const API_BASE_URL = "https://phantoms-api.onrender.com/api/v1"; // Replace with your backend URL
   const raffleDeposit = 150; // Pre-set to 150 phntm
+
+  // Fetch results from the backend
+  const fetchResults = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/raffle-results`);
+      setRaffleResults(response.data.results);
+    } catch (error) {
+      console.error("Error fetching raffle results:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchResults();
+  }, []);
 
   const handleDeposit = async () => {
     setDisabled(true);
@@ -130,6 +145,23 @@ const Raffle = ({ accountAddress }) => {
     setDisplayParticipants(!displayParticipants);
   };
 
+  // Function to handle social media sharing
+  const shareOnSocialMedia = (platform) => {
+    const url = encodeURIComponent(window.location.href);
+    const text = encodeURIComponent(
+      "I just entered the @PhantomPaals Raffle!\nJoin me at"
+    );
+    const hashtags = encodeURIComponent("PhantomPals,Raffle,Algorand");
+
+    if (platform === "twitter") {
+      window.open(
+        `https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=${hashtags}`,
+        "_blank"
+      );
+    }
+    // Add more platforms as needed
+  };
+
   // Adjusted the UI for depositing and registering
   return (
     <div className="flex flex-col items-center justify-center gap-y-2 p-4 lg:gap-x-2 lg:p-4">
@@ -163,150 +195,166 @@ const Raffle = ({ accountAddress }) => {
           }
         `}
       </style>
-      {
-        <>
-          <div className="p-8 flex flex-col items-center justify-center">
-            <div className="p-8 bg-white border-4 border-gray-600 rounded">
-              <div className="items-center text-center">
-                <h1 className="m-2 p-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-500">
-                  Raffle
-                </h1>
-                <h2
-                  style={{ wordBreak: "break-word", maxWidth: "20em" }}
-                  className="p-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-yellow-400 to-red-500"
-                >
-                  Draw Date: 11th May 2024
-                </h2>
-                <a
-                  href="https://t.co/aiJqcXi2Ae"
-                  className="text-blue-500 hover:underline dark:text-blue-400"
-                >
-                  Join our Discord for draw results and more information.
-                </a>
-                <a
-                  href="https://allo.info/asset/1238044254/nft"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="hover:underline dark:text-blue-400"
-                >
-                  <h3 className="pt-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-black to-black">
-                    Phantom #54
-                  </h3>
-                  <img
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      border: "2px solid white",
-                      borderRadius: "10px",
-                      maxHeight: "20em",
-                      maxWidth: "24em",
-                      paddingTop: "1em",
-                    }}
-                    src={phantomsImg}
-                    alt="roadmap"
-                  />
-                </a>
-
-                <br></br>
-                <div className="flex flex-col justify-end m-2 p-2">
-                  <div className="bg-yellow-400 m-2 p-2 text-black font-extrabold">
-                    150 PHNTM
-                  </div>
-                  {accountAddress ? (
-                    <button
-                      disabled={disabled}
-                      className="btn btn-primary m-2 p-2"
-                      onClick={handleDeposit}
-                    >
-                      Enter
-                    </button>
-                  ) : (
-                    <span className="pt-4 text-red-400 animate-pulse">
-                      To Enter, Ensure your wallet is connected
-                    </span>
-                  )}
-                </div>
-                <div
-                  style={{
-                    maxHeight: "20em",
-                    maxWidth: "24em",
-                  }}
-                >
-                  {showLoader && (
-                    <span className="loading loading-spinner loading-lg text-black">
-                      <div className="flex items-center justify-center">
-                        Processing ...
-                      </div>
-                    </span>
-                  )}
-                  <h1 className="m-2 p-2 text-black">{status}</h1>
-                  <h1
-                    style={{ wordBreak: "break-word" }}
-                    className="m-2 p-2 text-black"
-                  >
-                    {message}
-                  </h1>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="mb-2">
-            <button
-              className="btn btn-primary m-2 p-2"
-              onClick={toggleDisplayParticipants}
+      <div className="flex flex-col items-center justify-center">
+        <div className="p-8 bg-gradient-to-br from-gray-700 to-gray-600 border-4 border-gray-600 rounded">
+          <div className="items-center text-center">
+            <h1
+              style={{ fontFamily: "trebuchet ms" }}
+              className="m-2 p-2 text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-teal-400 to-teal-500"
             >
-              Display Participants
-            </button>
-          </div>
-          {displayParticipants && (
-            <div className="m-8 p-8 flex flex-col items-center justify-center">
-              <div className="p-8 bg-white border-4 border-gray-600 rounded">
-                <div className="items-center text-center">
-                  <h1 className="m-2 p-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-500">
-                    Participants
-                  </h1>
-                  <div className="participants">
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th>#</th>
-                          <th>Address</th>
-                          <th>Entry ID</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {participants.map((participant, index) => (
-                          <tr key={index}>
-                            <th>{index + 1}</th>
-                            <td>
-                              {`${participant?.participantAddress}`.substring(
-                                0,
-                                4
-                              )}
-                              ...
-                              {`${participant?.participantAddress}`.substring(
-                                `${participant?.participantAddress}`.length - 4,
-                                `${participant?.participantAddress}`.length
-                              )}
-                            </td>
-                            <td>
-                              {`${participant?.entryId}`.substring(0, 12)}...
-                              {`${participant?.entryId}`.substring(
-                                `${participant?.entryId}`.length - 12,
-                                `${participant?.entryId}`.length
-                              )}
-                            </td>
-                          </tr>
-                        ))}
-                      </tbody>
-                    </table>
+              Raffle
+            </h1>
+            <h2
+              style={{ wordBreak: "break-word", maxWidth: "20em" }}
+              className="p-2 text-xl font-extrabold text-yellow-300"
+            >
+              Draw Date: 11th May 2024
+            </h2>
+            <a
+              href="https://allo.info/asset/1238044254/nft"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline dark:text-blue-400"
+            >
+              <h3 className="pt-2 pb-4 text-xl font-extrabold text-white">
+                Phantom #54
+              </h3>
+              <img
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  border: "2px solid white",
+                  borderRadius: "10px",
+                  maxHeight: "20em",
+                  maxWidth: "24em",
+                }}
+                src={phantomsImg}
+                alt="roadmap"
+              />
+            </a>
+
+            <br></br>
+            <div className="flex flex-col justify-end m-2 p-2">
+              <div className="bg-yellow-400 m-2 p-2 text-black font-extrabold">
+                150 PHNTM
+              </div>
+              {accountAddress && raffleResults.length === 0 ? (
+                <button
+                  disabled={disabled}
+                  className="btn btn-primary m-2 p-2"
+                  onClick={handleDeposit}
+                >
+                  Enter
+                </button>
+              ) : raffleResults.length > 0 ? (
+                <span className="pt-4 text-xl text-red-300 animate-pulse">
+                  Raffle has ended
+                </span>
+              ) : (
+                <span className="pt-4 text-red-400 animate-pulse">
+                  To Enter, Ensure your wallet is connected
+                </span>
+              )}
+            </div>
+            <div
+              style={{
+                maxHeight: "20em",
+                maxWidth: "24em",
+              }}
+            >
+              {showLoader && (
+                <span className="loading loading-spinner loading-lg text-white">
+                  <div className="flex items-center justify-center">
+                    Processing ...
                   </div>
-                </div>
+                </span>
+              )}
+              <h1 className="m-2 p-2 text-white">{status}</h1>
+              {status === "Registered" && (
+                <button
+                  class="bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 border border-blue-900 rounded"
+                  onClick={() => shareOnSocialMedia("twitter")}
+                >
+                  Share on Twitter
+                </button>
+              )}
+              <h1
+                style={{ wordBreak: "break-word" }}
+                className="m-2 p-2 text-white"
+              >
+                {message}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      {raffleResults.length === 0 && (
+        <div className="mb-2">
+          <button
+            className="btn btn-primary m-2 p-2"
+            onClick={toggleDisplayParticipants}
+          >
+            Display Participants
+          </button>
+        </div>
+      )}
+      {displayParticipants && (
+        <div className="m-8 p-8 flex flex-col items-center justify-center">
+          <div className="p-8 bg-white border-4 border-gray-600 rounded">
+            <div className="items-center text-center">
+              <h1 className="m-2 p-2 text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-500">
+                Participants
+              </h1>
+              <div className="participants">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Address</th>
+                      <th>Entry ID</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {participants.map((participant, index) => (
+                      <tr key={index}>
+                        <th>{index + 1}</th>
+                        <td>
+                          {`${participant?.participantAddress}`.substring(0, 4)}
+                          ...
+                          {`${participant?.participantAddress}`.substring(
+                            `${participant?.participantAddress}`.length - 4,
+                            `${participant?.participantAddress}`.length
+                          )}
+                        </td>
+                        <td>
+                          {`${participant?.entryId}`.substring(0, 12)}...
+                          {`${participant?.entryId}`.substring(
+                            `${participant?.entryId}`.length - 12,
+                            `${participant?.entryId}`.length
+                          )}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
             </div>
-          )}
-        </>
-      }
+          </div>
+        </div>
+      )}
+      {raffleResults.length > 0 && (
+        <div
+          className="m-8 p-8 flex flex-col items-center justify-center bg-white border-4 border-gray-600 rounded text-black"
+          style={{ wordBreak: "break-word", maxWidth: "28em" }}
+        >
+          <h1 className="text-red-600 text-xl">Raffle Results</h1>
+          <ul>
+            {raffleResults.map((result, index) => (
+              <li key={index}>{`Winner Address: ${result.winnerAddress}`}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
