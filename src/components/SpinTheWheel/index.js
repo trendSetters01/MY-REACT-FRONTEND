95bca3cdfa289d68a6cabff87cc8fc42661692a6
trendSetters01 <trendycrypto01@gmail.com>
2024-05-13 03:26:02 -0400

    refactor: spin the wheel checks nft or phntm holdings

--- src/components/SpinTheWheel/index.js
+++ src/components/SpinTheWheel/index.js
@@ -26,11 +26,11 @@ export default function SpinTheWheel({ accountAddress }) {
   const [winReceipt, setWinReceipt] = useState("");
   const [gameOver, setGameOver] = useState(false);
   const [showLoader, setShowLoader] = useState(false);
+  const [showEligibilityLoader, setEligibilityLoader] = useState(false);
   const [rewardAccountBalance, setRewardAccountBalance] = useState(false);
   const [transactionId, setTransactionId] = useState(null);
   const [message, setMessage] = useState(null);
   const ref = useRef(null);
-
   const peraWallet = useContext(PeraWalletContext);
   const API_BASE_URL = "https://phantoms-api.onrender.com/api/v1"; // Replace with your backend URL
 
@@ -141,26 +141,32 @@ export default function SpinTheWheel({ accountAddress }) {
   }, [accountAddress]);
 
   const checkEligibility = async () => {
-    setShowLoader(true);
+    setEligibilityLoader(true);
     try {
       const response = await axios.post(`${API_BASE_URL}/check-participant`, {
         to: accountAddress,
       });
       const statucCode = response?.data?.statusCode;
+      if (statucCode === 417) {
+        setMessage(
+          `You need a Phantom Pals NFT or 1000 $PHNTM tokens to participate. See you soon!`
+        );
+        setGameOver(true);
+      }
       if (statucCode === 429) {
         setMessage(
           `Only one spin per wallet address, per day. Please Try again tomorrow!`
         );
         setGameOver(true);
       }
-      setShowLoader(false);
+      setEligibilityLoader(false);
     } catch (err) {
       console.log("Error checking eligibility:", err);
       setMessage(
         `Sorry sonething went wrong, please try again later or contact us on discord or email us at support@phantomshub.com.`
       );
       setGameOver(true);
-      setShowLoader(false);
+      setEligibilityLoader(false);
     }
   };
 
@@ -217,76 +223,91 @@ export default function SpinTheWheel({ accountAddress }) {
               minTime={5000}
             />
           </div>
-          <div className="flex flex-col items-center justify-center pb-4">
-            {/* Game Messages */}
-            {message && <p className="mt-4 mb-4 px-12 py-2">{message}</p>}
-            {!rewardAccountBalance && (
-              <p className="mt-2 mb-4 px-12 py-2">
-                We apologize for the incovinience but currently our reward
-                account wallet is running low in balance. We will refill it
-                soon. Thanks for your patience.
-              </p>
-            )}
-            {transactionId && (
-              <div className="flex flex-col items-center justify-center">
-                <a
-                  href={`https://allo.info/tx/${winReceipt}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="p-2 mt-4 text-white hover:underline"
-                  style={{ wordBreak: "break-word" }}
-                >
-                  Phantom Pals Win Confirmation receipt- txID: {winReceipt}
-                </a>
-                <p className="p-2" style={{ wordBreak: "break-word" }}>
-                  keep this for your records or to claim your reward later
-                  through the discord channel if anything goes wrong.
+          {showEligibilityLoader ? (
+            <span className="m-2 loading loading-spinner loading-lg text-white">
+              <div className="flex justify-center pb-4">
+                <svg
+                  className="animate-spin h-5 w-5 mr-3 ..."
+                  style={{ border: "5px solid white" }}
+                  viewBox="0 0 24 24"
+                ></svg>
+                <p style={{ wordBreak: "break-word" }}>
+                  Checking eligibility for: {accountAddress}
                 </p>
               </div>
-            )}
-            {showLoader && (
-              <span className="m-2 loading loading-spinner loading-lg text-white">
-                <div className="flex justify-center pb-4">
-                  <svg
-                    className="animate-spin h-5 w-5 mr-3 ..."
-                    style={{ border: "5px solid white" }}
-                    viewBox="0 0 24 24"
-                  ></svg>
-                  Processing ...
+            </span>
+          ) : (
+            <div className="flex flex-col items-center justify-center pb-4">
+              {/* Game Messages */}
+              {message && <p className="mt-4 mb-4 px-12 py-2">{message}</p>}
+              {!rewardAccountBalance && (
+                <p className="mt-2 mb-4 px-12 py-2">
+                  We apologize for the incovinience but currently our reward
+                  account wallet is running low in balance. We will refill it
+                  soon. Thanks for your patience.
+                </p>
+              )}
+              {transactionId && (
+                <div className="flex flex-col items-center justify-center">
+                  <a
+                    href={`https://allo.info/tx/${winReceipt}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="p-2 mt-4 text-white hover:underline"
+                    style={{ wordBreak: "break-word" }}
+                  >
+                    Phantom Pals Win Confirmation receipt- txID: {winReceipt}
+                  </a>
+                  <p className="p-2" style={{ wordBreak: "break-word" }}>
+                    keep this for your records or to claim your reward later
+                    through the discord channel if anything goes wrong.
+                  </p>
                 </div>
-              </span>
-            )}
-            {transactionId && (
-              <a
-                href={`https://allo.info/tx/${transactionId}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="p-2 mt-4 text-black hover:underline"
-                style={{ wordBreak: "break-word" }}
+              )}
+              {showLoader && (
+                <span className="m-2 loading loading-spinner loading-lg text-white">
+                  <div className="flex justify-center pb-4">
+                    <svg
+                      className="animate-spin h-5 w-5 mr-3 ..."
+                      style={{ border: "5px solid white" }}
+                      viewBox="0 0 24 24"
+                    ></svg>
+                    Processing ...
+                  </div>
+                </span>
+              )}
+              {transactionId && (
+                <a
+                  href={`https://allo.info/tx/${transactionId}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="p-2 mt-4 text-black hover:underline"
+                  style={{ wordBreak: "break-word" }}
+                >
+                  Transaction ID: {transactionId}
+                </a>
+              )}
+              <button
+                className="bg-gradient-to-r from-red-400 to-red-800 px-8 py-4 rounded mx-2"
+                disabled={gameOver}
+                onClick={() => {
+                  setGameOver(true);
+                  ref.current.handleSpin();
+                  setTimeout(() => {
+                    setShowConfetti(true);
+                    if (accountAddress) {
+                      handleClaimReward();
+                    }
+                  }, 3000);
+                  setTimeout(() => {
+                    setShowConfetti(false);
+                  }, 13000);
+                }}
               >
-                Transaction ID: {transactionId}
-              </a>
-            )}
-            <button
-              className="bg-gradient-to-r from-red-400 to-red-800 px-8 py-4 rounded mx-2"
-              disabled={gameOver}
-              onClick={() => {
-                setGameOver(true);
-                ref.current.handleSpin();
-                setTimeout(() => {
-                  setShowConfetti(true);
-                  if (accountAddress) {
-                    handleClaimReward();
-                  }
-                }, 3000);
-                setTimeout(() => {
-                  setShowConfetti(false);
-                }, 13000);
-              }}
-            >
-              Spin
-            </button>
-          </div>
+                Spin
+              </button>
+            </div>
+          )}
         </>
       )}
     </div>
